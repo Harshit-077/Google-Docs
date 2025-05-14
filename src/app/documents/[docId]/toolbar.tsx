ebe1cd0f7b84dfb1f0abca3ae52ce010dd02421b
Harshit-077 <harshitsharma.0777@gmail.com>
2025-05-14 13:19:09 +0530

    added text-highlight

--- src/app/documents/[docId]/toolbar.tsx
+++ src/app/documents/[docId]/toolbar.tsx
@@ -12,6 +12,7 @@ import {
     ChevronDownIcon,
     ListTodoIcon,
     RemoveFormattingIcon,
+    HighlighterIcon,
 
 } from "lucide-react";
 import { cn } from "@/lib/utils";
@@ -26,6 +27,31 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+
+const HighlightColorButton = () => {
+    const { editor } = useEditorStore();
+    const onChange = (color: ColorResult) => {
+        editor?.chain().focus().setHighlight({color: color.hex}).run();
+    };
+    const value = editor?.getAttributes("highlight").color || "#FFFFFF";
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <HighlighterIcon className="size-4"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0">
+                <SketchPicker 
+                    color={value}
+                    onChange={onChange}
+                />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
 
 const TextColorButton = () => {
     const { editor } = useEditorStore();
@@ -262,7 +288,7 @@ export const Toolbar = () => {
                 <ToolbarButton key={item.label} {...item} />
             ))}
             <TextColorButton />
-            {/* Highlight Color */}
+            <HighlightColorButton />
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
             {/* Link */}
             {/* Image */}
