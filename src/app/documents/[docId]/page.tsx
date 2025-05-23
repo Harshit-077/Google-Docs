8a90ec0e628b4b6b3e6a15a9874b2a971aa07b12
Harshit-077 <harshitsharma.0777@gmail.com>
2025-05-23 22:48:27 +0530

    Basic Structure for proper navbar ready

--- src/app/documents/[docId]/page.tsx
+++ src/app/documents/[docId]/page.tsx
@@ -2,15 +2,19 @@ import { Editor } from "./editor";
 import { Navbar } from "./navbar";
 import { Toolbar } from "./toolbar";
 interface DocumentPageProps {
-    params: Promise< {docId: string} >;
+    params: Promise<{ docId: string }>;
 }
-const nested = async ({ params } : DocumentPageProps) => {
+const nested = async ({ params }: DocumentPageProps) => {
     const { docId } = await params;
     return (
         <div className="min-h-screen bg-[#fafbfd]">
-            <Navbar />
-            <Toolbar/>
-            <Editor />
+            <div className="flex flex-col px-4 pt-2 gap-2 fixed top-0 left-0 right-0 bg-[#fafbfd] z-10 print:hidden">
+                <Navbar />
+                <Toolbar />
+            </div>
+            <div className="pt-[114px] print:pt-0">
+                <Editor />
+            </div>
         </div>
     );
 }
