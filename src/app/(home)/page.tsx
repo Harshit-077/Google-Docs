b5d8c7c2417c05cb8ef38333b4045cdd1fa46b1a
Harshit-077 <harshitsharma.0777@gmail.com>
2025-05-27 13:01:00 +0530

    Convex Database Integration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,28 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+"use client";
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./template-gallery";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
-        <Navbar />
-      </div>
-      <div className="mt-16">
-        <TemplateGallery />
-         
-      </div>
-    </div>
-  );
+	const documents = useQuery(api.documents.get)
+	if(documents === undefined) {
+		return <p>Loading...</p>;
+	}
+
+
+	return (
+		<div className="min-h-screen flex flex-col">
+			<div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+				<Navbar />
+			</div>
+			<div className="mt-16">
+				<TemplateGallery />
+				{documents?.map((document) => (
+					<span key = {document._id}>{document.title}</span>
+				))}
+			</div>
+		</div>
+	);
 }
 export default Home;
