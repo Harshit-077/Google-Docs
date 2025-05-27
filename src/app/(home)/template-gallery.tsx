ec72b5b50c7d473778ccd678914ee73a423b4c16
Harshit-077 <harshitsharma.0777@gmail.com>
2025-05-27 16:10:28 +0530

    enhanced UI

--- src/app/(home)/template-gallery.tsx
+++ src/app/(home)/template-gallery.tsx
@@ -9,11 +9,30 @@ import {
 } from "@/components/ui/carousel";
 import { templates } from "@/constants/templates";
 import { cn } from "@/lib/utils";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
 
 
 export const TemplateGallery = () => {
-    const isCreating = false;
+    const router = useRouter();
+    const create = useMutation(api.documents.post);
+    const [isCreating,setIsCreating] = useState(false);
 
+    const onTemplateClick = (title: string, initialContent: string) => {
+        setIsCreating(true);
+        create({ title, initialContent })
+            .then((documentId) => {
+                router.push(`/documents/${documentId}`);
+            })
+            .catch((error) => {
+                console.error("Error creating document:", error);
+            })
+            .finally(() => {
+                setIsCreating(false);
+            });
+    }
     return (
         <div className="bg-[#f1f3f4]">
             <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
@@ -33,7 +52,7 @@ export const TemplateGallery = () => {
                                 >
                                     <button
                                         disabled={isCreating}
-                                        onClick={() => { }}
+                                        onClick={() => onTemplateClick(template.label, "")}
                                         style={{
                                             backgroundImage: `url(${template.imageUrl})`,
                                             backgroundSize: "cover",
